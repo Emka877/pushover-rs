26c37517220ccb2bd0b120e44e6df8745e8a0511
Oscuro87 <capelle.e@gmail.com>
2021-11-02 17:26:46 +0100

    Make MessageBuilder more easily accessible

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-mod pushover;
 #[cfg(test)]
 mod tests;
+mod pushover;
 
 use pushover::{constants::PUSHOVER_API_ENDPOINT, data::{Message, PushoverResponse}};
+pub use pushover::data::MessageBuilder;
 
-pub async fn send_request(message: Message) -> Result<PushoverResponse, Box<dyn std::error::Error>> {
+pub async fn send_pushover_request(message: Message) -> Result<PushoverResponse, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let response = client
         .post(PUSHOVER_API_ENDPOINT)
