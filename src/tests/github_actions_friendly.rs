ac051079bc73cf7e61b9e865a986ffa33b5a060d
Oscuro87 <capelle.e@gmail.com>
2021-11-09 17:09:45 +0100

    tests for last commit

--- src/tests/github_actions_friendly.rs
+++ src/tests/github_actions_friendly.rs
@@ -1,3 +1,7 @@
+use std::vec;
+
+use serde::de::IntoDeserializer;
+
 use crate::{AttachmentMessageBuilder, Message, MessageBuilder, PushoverSound};
 
 #[test]
@@ -18,7 +22,9 @@ pub fn test_message_builder() {
         .set_priority(100) // "Error": Out of the [-2, 2] boundary -> Should be reset to 0
         .set_sound(PushoverSound::CASHREGISTER)
         .set_timestamp(1635861224)
-        .add_device("home desktop")
+        .add_device("device a")
+        .add_device("device b")
+        .add_device("device a") // Duplicate, should not be added
         .build();
 
     assert_eq!(mfull.user_key, "abc".to_owned());
@@ -30,7 +36,56 @@ pub fn test_message_builder() {
     assert_eq!(mfull.priority, Some(0));
     assert_eq!(mfull.sound, Some("cashregister".to_owned()));
     assert_eq!(mfull.timestamp, Some(1635861224));
-    assert_eq!(mfull.devices, Some(vec!["home desktop".to_owned()]));
+    assert_eq!(mfull.devices.as_ref().unwrap().len(), 2 as usize);
+    assert_eq!(mfull.devices, Some(vec!["device a".to_owned(), "device b".to_owned()]));
+
+    /* Test devices list */
+        /* Test set devices list */
+    let devices_list: Vec<&str> = vec![
+        "device a",
+        "device b",
+        "device c",
+    ];
+    let mnext: Message = MessageBuilder::new("abc", "def", "testing")
+        .set_devices(devices_list.clone())
+        .build();
+    let list_from_mnext: Vec<String> = mnext.devices.unwrap();
+    
+    // It's ok to compare Vec<String> with Vec<&str>
+    assert_eq!(list_from_mnext, devices_list);
+    
+        /* Test merge devices list */
+    let devices_list_1: Vec<&str> = vec![
+        "device a",
+        "device b",
+    ];
+    let devices_list_2: Vec<&str> = vec![
+        "device b",
+        "device c",
+        "device a",
+    ];
+    let expected: Vec<String> = vec! [
+        "device a".to_owned(),
+        "device b".to_owned(),
+        "device c".to_owned(),
+    ];
+    let mnext_merge: Message = MessageBuilder::new("abc", "def", "testing")
+        .set_devices(devices_list_1)
+        .merge_devices(devices_list_2)
+        .build();
+    let list_from_mnext_merge: Vec<String> = mnext_merge.devices.unwrap();
+    assert_eq!(list_from_mnext_merge, expected);
+
+        /* Test clear devices list */
+    let devices_list_toclear: Vec<&str> = vec![
+        "device a",
+        "device b",
+    ];
+    let mnext_clear: Message = MessageBuilder::new("abc", "def", "testing")
+        .set_devices(devices_list_toclear)
+        .clear_devices_list()
+        .build();
+    assert_eq!(mnext_clear.devices, None);
 }
 
 #[test]
