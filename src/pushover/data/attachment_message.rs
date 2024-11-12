08a768e104fe8d4694199dea7fabf6ea8b975973
Oscuro87 <capelle.e@gmail.com>
2024-11-12 19:01:55 +0100

    Update dependencies; TTL parameter must be either None or greater than 0 -> Stopped defaulting TTL to 0.

--- src/pushover/data/attachment_message.rs
+++ src/pushover/data/attachment_message.rs
@@ -1,6 +1,7 @@
 use reqwest::blocking::multipart::Form;
+use serde::Serialize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 /**
  A message containing an attachment, to be used in conjuction with the send_pushover_request_with_attachment function.
 
@@ -19,27 +20,34 @@ pub struct AttachmentMessage {
 
     /* Optional */
     /// The title of the message, otherwise your app's name will be used
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub title: Option<String>,
     /// A supplementary URL to show with your message
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub url: Option<String>,
     /// A title for your supplementary URL, otherwise just the URL is shown
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub url_title: Option<String>,
     /// Send as -2 to generate no notification/alert, -1 to always send as a quiet notification, 1 to display as high-priority and bypass the user's quiet hours, or 2 to also require confirmation from the user
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub priority: Option<String>,
     /// The name of one of the sounds supported by device clients to override the user's default sound choice. (See sound list: https://pushover.net/api#sounds)
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub sound: Option<String>,
     /// A Unix timestamp of your message's date and time to display to the user, rather than the time your message is received by our API
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub timestamp: Option<String>, // Year 2038 proof :p
     /// A list of device names to send the push notifications to, if you want to limit the notification to certain devices.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub devices: Option<Vec<String>>,
     /// A TTL (Time to Live) in seconds, after which the message will be automatically deleted from the recipient's inbox.
-    /// Setting *ttl* to None or 0 prevents this auto removal.
+    /// Setting *ttl* to None prevents this auto removal. Setting TTL to 0 will raise an error (ttl must be > 0).
     pub ttl: Option<u32>,
 }
 
 impl AttachmentMessage {
     pub fn into_form(self) -> Result<Form, std::io::Error> {
-        Form::new()
+        let mut form = Form::new()
             .text("token", self.app_token.clone())
             .text("user", self.user_key.clone())
             .text("message", self.message.clone())
@@ -49,9 +57,12 @@ impl AttachmentMessage {
             .text("priority", self.priority.unwrap_or("".into()))
             .text("sound", self.sound.clone().unwrap_or("".into()))
             .text("timestamp", self.timestamp.unwrap_or("".into()))
-            .text("device", self.devices.clone().unwrap_or(vec!()).join(","))
-            .text("ttl", self.ttl.unwrap_or(0).to_string())
-            .file("attachment", self.attachment.clone())
+            .text("device", self.devices.clone().unwrap_or(vec!()).join(","));
+        // TTL became required if it has a value, 0 doesn't work anymore.
+        if self.ttl.is_some() {
+            form = form.text("ttl", self.ttl.unwrap_or(999).to_string());
+        }
+        form.file("attachment", self.attachment.clone())
     }
 }
 
